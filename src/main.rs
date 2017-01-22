907daca9f25bb354a4d740aa8ab76848e8618683
Pedro Côrte-Real <pedro@pedrocr.net>
2017-01-22 22:31:35 +0000

    implement async image loading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use] extern crate conrod;
 use conrod::{widget, Colorable, Positionable, Sizeable, Widget, color};
-use conrod::backend::piston::gfx::{G2dTexture, Texture, TextureSettings};
+use conrod::backend::piston::gfx::{Texture, TextureSettings};
 use conrod::backend::piston::{self, Window, WindowEvents, OpenGL};
 use conrod::backend::piston::draw::ImageSize;
 use conrod::backend::piston::event::UpdateEvent;
@@ -8,12 +8,19 @@ use conrod::backend::piston::event::UpdateEvent;
 extern crate gfx_device_gl;
 
 extern crate image;
-use image::ImageBuffer;
+use image::{ImageBuffer, Rgba};
 
 extern crate rawloader;
 use std::env;
-use std::thread;
 use std::sync::RwLock;
+use std::collections::HashMap;
+#[macro_use] extern crate lazy_static;
+
+extern crate crossbeam;
+
+lazy_static! {
+  static ref ILOCK: RwLock<HashMap<String, ImageBuffer<Rgba<u8>, Vec<u8>>>> = RwLock::new(HashMap::new());
+}
 
 fn main() {
   let args: Vec<_> = env::args().collect();
@@ -48,85 +55,88 @@ fn main() {
   widget_ids!(struct Ids { background, raw_image });
   let ids = Ids::new(ui.widget_id_generator());
 
-  let wlock = RwLock::new(window);
-  let ilock = RwLock::new(conrod::image::Map::new());
-  //thread::spawn(||
-  {
-    let img = load_image(&wlock, &file);
-    let mut image_map = ilock.write().unwrap();
-    image_map.insert(ids.raw_image, img);
-  }//);
+  let mut image_map = conrod::image::Map::new();
 
-  // Poll events from the window.
-  loop {
-    let mut window = wlock.write().unwrap();
-    match (*window).next_event(&mut events) {
-      None => break,
-      Some(event) => {
-        // Convert the piston event to a conrod input event.
-        if let Some(e) = piston::window::convert_event(event.clone(), &window) {
-            ui.handle_event(e);
+
+  crossbeam::scope(|scope| {
+    load_image(&file, scope);
+
+    // Poll events from the window.
+    while let Some(event) = window.next_event(&mut events) {
+      // Convert the piston event to a conrod input event.
+      if let Some(e) = piston::window::convert_event(event.clone(), &window) {
+          ui.handle_event(e);
+      }
+
+      window.draw_2d(&event, |c, g| {
+        if let Some(primitives) = ui.draw_if_changed() {
+          fn texture_from_image<T>(img: &T) -> &T { img };
+          piston::window::draw(c, g, primitives,
+                                               &mut text_texture_cache,
+                                               &image_map,
+                                               texture_from_image);
         }
+      });
 
-        let image_map = ilock.read().unwrap();
+      let (width,height) = window.window.window.get_inner_size_pixels().unwrap();
+      let mut width = width as f64;
+      let mut height = height as f64;
 
-        window.draw_2d(&event, |c, g| {
-          if let Some(primitives) = ui.draw_if_changed() {
-            fn texture_from_image<T>(img: &T) -> &T { img };
-            piston::window::draw(c, g, primitives,
-                                                 &mut text_texture_cache,
-                                                 &image_map,
-                                                 texture_from_image);
-          }
-        });
-
-        let (width,height) = window.window.window.get_inner_size_pixels().unwrap();
-        let mut width = width as f64;
-        let mut height = height as f64;
-
-        match image_map.get(&ids.raw_image) {
+      if image_map.get(&ids.raw_image).is_none() {
+        let image_cache = ILOCK.read().unwrap();
+        match image_cache.get(file) {
           None => {},
-          Some(img) => {
-            let (maxw, maxh) = img.get_size();
-            let scale = (maxw as f64)/(maxh as f64);
-            width = width.min(maxw as f64);
-            height = height.min(maxh as f64);
-            if width/height > scale {
-              width = height * scale;
-            } else {
-              height = width / scale;
-            }
+          Some(imgbuf) => {
+            let settings = TextureSettings::new();
+            let factory = &mut window.context.factory;
+            let img = Texture::from_image(factory, &imgbuf, &settings).unwrap();
+            image_map.insert(ids.raw_image, img);
           },
         }
-
-        event.update(|_| {
-          let ui = &mut ui.set_widgets();
-          // Draw a light blue background.
-          widget::Canvas::new().color(color::BLACK).set(ids.background, ui);
-          // Instantiate the `Image` at its full size in the middle of the window.
-          widget::Image::new().w_h(width, height).middle().set(ids.raw_image, ui);
-        });
       }
+
+      match image_map.get(&ids.raw_image) {
+        None => {},
+        Some(img) => {
+          let (maxw, maxh) = img.get_size();
+          let scale = (maxw as f64)/(maxh as f64);
+          width = width.min(maxw as f64);
+          height = height.min(maxh as f64);
+          if width/height > scale {
+            width = height * scale;
+          } else {
+            height = width / scale;
+          }
+        },
+      }
+
+      event.update(|_| {
+        let ui = &mut ui.set_widgets();
+        // Draw a light blue background.
+        widget::Canvas::new().color(color::BLACK).set(ids.background, ui);
+        // Instantiate the `Image` at its full size in the middle of the window.
+        widget::Image::new().w_h(width, height).middle().set(ids.raw_image, ui);
+      });
     }
-  }
+  });
 }
 
 // Load the image from a file
-fn load_image(wlock: &RwLock<Window>, path: &str) -> G2dTexture<'static> {
-  let decoded = rawloader::decode(path).unwrap().to_rgb(0, 0).unwrap();
+fn load_image<'a>(path: &'a str, scope: &crossbeam::Scope<'a>) {
+  let file = path.to_string();
 
-  // Convert f32 RGB into u8 RGBA
-  let mut buffer = vec![0 as u8; (decoded.width*decoded.height*4) as usize];
-  for (pixin, pixout) in decoded.data.chunks(3).zip(buffer.chunks_mut(4)) {
-    pixout[0] = (pixin[0]*255.0).max(0.0).min(255.0) as u8;
-    pixout[1] = (pixin[1]*255.0).max(0.0).min(255.0) as u8;
-    pixout[2] = (pixin[2]*255.0).max(0.0).min(255.0) as u8;
-    pixout[3] = 255;
-  }  
-  let imgbuf = ImageBuffer::from_raw(decoded.width as u32, decoded.height as u32, buffer).unwrap();
-
-  let settings = TextureSettings::new();
-  let mut window = wlock.write().unwrap();
-  let factory = &mut window.context.factory;
-  Texture::from_image(factory, &imgbuf, &settings).unwrap()
+  scope.spawn(move || {
+    let decoded = rawloader::decode(path).unwrap().to_rgb(0, 0).unwrap();
+    // Convert f32 RGB into u8 RGBA
+    let mut buffer = vec![0 as u8; (decoded.width*decoded.height*4) as usize];
+    for (pixin, pixout) in decoded.data.chunks(3).zip(buffer.chunks_mut(4)) {
+      pixout[0] = (pixin[0]*255.0).max(0.0).min(255.0) as u8;
+      pixout[1] = (pixin[1]*255.0).max(0.0).min(255.0) as u8;
+      pixout[2] = (pixin[2]*255.0).max(0.0).min(255.0) as u8;
+      pixout[3] = 255;
+    }
+    let img = ImageBuffer::from_raw(decoded.width as u32, decoded.height as u32, buffer).unwrap();
+    let mut image_cache = ILOCK.write().unwrap();
+    image_cache.insert(file, img);
+  });
 }

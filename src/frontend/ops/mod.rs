67b44e83f6abd405e585b87db14bb5a77703cc81
Pedro Côrte-Real <pedro@pedrocr.net>
2018-08-19 15:41:07 +0100

    Move each op's GUI into its own file

--- src/frontend/ops/mod.rs
+++ src/frontend/ops/mod.rs
@@ -1,21 +1,13 @@
 extern crate conrod;
-use conrod::{widget, Positionable, Sizeable, Widget};
+pub use conrod::{widget, UiCell, Positionable, Sizeable, Widget};
 extern crate imagepipe;
+pub use self::imagepipe::PipelineOps;
 
 use frontend::main::*;
 
-static ORIENTATION_NAMES: [&str; 8] = [
-  "Normal",
-  "HorizontalFlip",
-  "Rotate180",
-  "VerticalFlip",
-  "Transpose",
-  "Rotate90",
-  "Transverse",
-  "Rotate270",
-];
+mod transform;
 
-pub fn draw_gui(chimper: &mut Chimper, ui: &mut conrod::UiCell) -> bool {
+pub fn draw_gui(chimper: &mut Chimper, ui: &mut UiCell) -> bool {
   let mut needs_update = false;
   let mut ops = chimper.ops.lock().unwrap();
     let ids = match chimper.ids {
@@ -24,15 +16,7 @@ pub fn draw_gui(chimper: &mut Chimper, ui: &mut conrod::UiCell) -> bool {
   };
 
   if let Some((_, ref mut ops)) = *ops {
-    let orientation = ops.transform.orientation.to_u16() as usize;
-    for event in widget::drop_down_list::DropDownList::new(&ORIENTATION_NAMES, Some(orientation))
-      .w_h(130.0, 30.0)
-      .top_left_with_margin_on(ids.setcont, 6.0)
-      .set(ids.dropdown, ui)
-    {
-      ops.transform.orientation = imagepipe::Orientation::from_u16(event as u16);
-      needs_update = true;
-    }
+    needs_update = transform::draw_gui(ids, ui, ops) || needs_update;
   }
 
   needs_update
---
+++ src/frontend/ops/transform.rs
@@ -0,0 +1,28 @@
+use frontend::ops::*;
+
+static ORIENTATION_NAMES: [&str; 8] = [
+  "Normal",
+  "HorizontalFlip",
+  "Rotate180",
+  "VerticalFlip",
+  "Transpose",
+  "Rotate90",
+  "Transverse",
+  "Rotate270",
+];
+
+pub fn draw_gui(ids: &ChimperIds, ui: &mut UiCell, ops: &mut PipelineOps) -> bool {
+  let mut needs_update = false;
+
+  let orientation = ops.transform.orientation.to_u16() as usize;
+  for event in widget::drop_down_list::DropDownList::new(&ORIENTATION_NAMES, Some(orientation))
+    .w_h(130.0, 30.0)
+    .top_left_with_margin_on(ids.setcont, 6.0)
+    .set(ids.dropdown, ui)
+  {
+    ops.transform.orientation = imagepipe::Orientation::from_u16(event as u16);
+    needs_update = true;
+  }
+
+  needs_update
+}

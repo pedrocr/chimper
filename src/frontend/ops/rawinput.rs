4465b9ef3a1de50c1174397b701a4db346ec440e
Pedro Côrte-Real <pedro@pedrocr.net>
2018-08-25 21:56:58 +0100

    Add inputs for white and black raw levels

--- src/frontend/ops/rawinput.rs
+++ src/frontend/ops/rawinput.rs
@@ -39,7 +39,7 @@ fn get_patnum(ops: &PipelineOps) -> Option<usize> {
 pub fn draw_gui(ids: &mut ChimperIds, ui: &mut UiCell, ops: &mut PipelineOps, id: WidgetId) -> (bool, f64) {
   let mut needs_update = false;
 
-  ids.op_rawinput.resize(10, &mut ui.widget_id_generator());
+  ids.op_rawinput.resize(23, &mut ui.widget_id_generator());
   let id_pattern = ids.op_rawinput[0];
   let id_label_pattern = ids.op_rawinput[1];
 
@@ -63,6 +63,27 @@ pub fn draw_gui(ids: &mut ChimperIds, ui: &mut UiCell, ops: &mut PipelineOps, id
     needs_update = true;
   }
 
+  macro_rules! textbox_num_input {
+    ($parentwidget:expr, $xpos:expr, $ypos:expr, $widget:expr, $value:expr, $typ:ty) => {
+      for event in widget::text_box::TextBox::new(&($value.to_string()))
+        .right_justify()
+        .w_h(130.0, 30.0)
+        .top_left_with_margins_on($parentwidget, $ypos, $xpos)
+        .set($widget, ui)
+      {
+        if let widget::text_box::Event::Update(val) = event {
+          if val.trim() == "" {
+            $value = 0 as $typ;
+          }
+          if let Ok(val) = val.parse::<$typ>() {
+            $value = val;
+          }
+        }
+        needs_update = true;
+      }
+    };
+  }
+
   voffset += 36.0;
   let mut idnum = 0;
   macro_rules! crop_widget {
@@ -73,22 +94,7 @@ pub fn draw_gui(ids: &mut ChimperIds, ui: &mut UiCell, ops: &mut PipelineOps, id
         .top_left_with_margins_on(id, voffset, 12.0)
         .set(ids.op_rawinput[idnum], ui)
       ;
-      for event in widget::text_box::TextBox::new(&ops.gofloat.$value.to_string())
-        .right_justify()
-        .w_h(130.0, 30.0)
-        .top_left_with_margins_on(id, voffset, 156.0)
-        .set(ids.op_rawinput[idnum+1], ui)
-      {
-        if let widget::text_box::Event::Update(val) = event {
-          if val.trim() == "" {
-            ops.gofloat.$value = 0;
-          }
-          if let Ok(val) = val.parse::<usize>() {
-            ops.gofloat.$value = val;
-          }
-        }
-        needs_update = true;
-      }
+      textbox_num_input!(id, 156.0, voffset, ids.op_rawinput[idnum+1], ops.gofloat.$value, usize);
       voffset += 36.0;
     };
   }
@@ -98,6 +104,26 @@ pub fn draw_gui(ids: &mut ChimperIds, ui: &mut UiCell, ops: &mut PipelineOps, id
   crop_widget!("Crop Top",    crop_top);
   crop_widget!("Crop Bottom", crop_bottom);
 
+  macro_rules! range_widget {
+    ($name:expr, $idx:expr) => {
+      idnum += 3;
+      widget::primitive::text::Text::new($name)
+        .w_h(150.0, 30.0)
+        .top_left_with_margins_on(id, voffset, 12.0)
+        .set(ids.op_rawinput[idnum], ui)
+      ;
+
+      textbox_num_input!(id, 156.0, voffset, ids.op_rawinput[idnum+1], ops.level.blacklevels[$idx], f32);
+      textbox_num_input!(id, 306.0, voffset, ids.op_rawinput[idnum+2], ops.level.whitelevels[$idx], f32);
+
+      voffset += 36.0;
+    };
+  }
+
+  range_widget!("Red",     0);
+  range_widget!("Green",   1);
+  range_widget!("Blue",    2);
+  range_widget!("Emerald", 3);
 
   (needs_update, voffset)
 }

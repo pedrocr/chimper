f46a40cd496d66c2da597aa76761b808fc97d3ca
Pedro Côrte-Real <pedro@pedrocr.net>
2018-08-26 01:21:42 +0100

    Simplify management of widget ids

--- src/frontend/ops/rawinput.rs
+++ src/frontend/ops/rawinput.rs
@@ -39,37 +39,44 @@ fn get_patnum(ops: &PipelineOps) -> Option<usize> {
 pub fn draw_gui(ids: &mut ChimperIds, ui: &mut UiCell, ops: &mut PipelineOps, id: WidgetId) -> (bool, f64) {
   let mut needs_update = false;
 
-  ids.op_rawinput.resize(50, &mut ui.widget_id_generator());
+  let mut numids = 0;
+  macro_rules! new_widget {
+    () => {{
+      numids += 1;
+      ids.op_rawinput.resize(numids, &mut ui.widget_id_generator());
+      ids.op_rawinput[numids-1]
+    }}
+  }
 
   let mut voffset = 36.0 * 0.5;
   macro_rules! label {
-    ($width:expr, $xpos:expr, $widget:expr, $name: expr, $justify:expr) => {
+    ($width:expr, $xpos:expr, $name: expr, $justify:expr) => {
       widget::primitive::text::Text::new($name)
         .justify($justify)
         .w_h($width, 30.0)
         .top_left_with_margins_on(id, voffset+3.0, $xpos)
-        .set($widget, ui)
+        .set(new_widget!(), ui)
       ;
     };
   }
   macro_rules! left_label {
-    ($widget:expr, $name: expr) => {
-      label!(140.0, 0.0, $widget, $name, Justify::Right);
+    ($name: expr) => {
+      label!(140.0, 0.0, $name, Justify::Right);
     };
   }
   macro_rules! divider_label {
-    ($widget:expr, $name: expr) => {
-      label!(100.0, 0.0, $widget, $name, Justify::Right);
+    ($name: expr) => {
+      label!(100.0, 0.0, $name, Justify::Right);
     };
   }
 
-  divider_label!(ids.op_rawinput[0], "Demosaic");
+  divider_label!("Demosaic");
   voffset += 36.0 * 1.5;
-  left_label!(ids.op_rawinput[1], "Filter");
+  left_label!("Filter");
   for event in widget::drop_down_list::DropDownList::new(&PATTERNS, get_patnum(ops))
     .w_h(140.0, 30.0)
     .top_left_with_margins_on(id, voffset, 150.0)
-    .set(ids.op_rawinput[2], ui)
+    .set(new_widget!(), ui)
   {
     if event == 0 {
       ops.gofloat.is_cfa = false;
@@ -81,12 +88,12 @@ pub fn draw_gui(ids: &mut ChimperIds, ui: &mut UiCell, ops: &mut PipelineOps, id
   }
 
   macro_rules! textbox_num_input {
-    ($xpos:expr, $ypos:expr, $widget:expr, $value:expr, $typ:ty) => {
+    ($xpos:expr, $value:expr, $typ:ty) => {
       for event in widget::text_box::TextBox::new(&($value.to_string()))
         .center_justify()
         .w_h(80.0, 30.0)
-        .top_left_with_margins_on(id, $ypos, $xpos)
-        .set($widget, ui)
+        .top_left_with_margins_on(id, voffset, $xpos)
+        .set(new_widget!(), ui)
       {
         if let widget::text_box::Event::Update(val) = event {
           if val.trim() == "" {
@@ -102,27 +109,27 @@ pub fn draw_gui(ids: &mut ChimperIds, ui: &mut UiCell, ops: &mut PipelineOps, id
   }
 
   voffset += 36.0 * 1.5;
-  divider_label!(ids.op_rawinput[3], "Crops");
+  divider_label!("Crops");
   voffset += 36.0 * 0.5;
   voffset += 36.0;
-  left_label!(ids.op_rawinput[4], "Top");
-  textbox_num_input!(150.0, voffset, ids.op_rawinput[5], ops.gofloat.crop_top, usize);
+  left_label!("Top");
+  textbox_num_input!(150.0, ops.gofloat.crop_top, usize);
 
   voffset += 36.0;
-  left_label!(ids.op_rawinput[6], "Left/Right");
-  textbox_num_input!(150.0, voffset, ids.op_rawinput[7], ops.gofloat.crop_left, usize);
-  textbox_num_input!(250.0, voffset, ids.op_rawinput[8], ops.gofloat.crop_right, usize);
+  left_label!("Left/Right");
+  textbox_num_input!(150.0, ops.gofloat.crop_left, usize);
+  textbox_num_input!(250.0, ops.gofloat.crop_right, usize);
 
   voffset += 36.0;
-  left_label!(ids.op_rawinput[9], "Bottom");
-  textbox_num_input!(150.0, voffset, ids.op_rawinput[10], ops.gofloat.crop_bottom, usize);
+  left_label!("Bottom");
+  textbox_num_input!(150.0, ops.gofloat.crop_bottom, usize);
 
   macro_rules! slider_input {
-    ($xpos:expr, $ypos:expr, $widget:expr, $value:expr, $min:expr, $max:expr, $typ:ty) => {
+    ($xpos:expr, $value:expr, $min:expr, $max:expr, $typ:ty) => {
       for event in widget::slider::Slider::new($value, $min, $max)
         .w_h(170.0, 30.0)
-        .top_left_with_margins_on(id, $ypos, $xpos)
-        .set($widget, ui)
+        .top_left_with_margins_on(id, voffset, $xpos)
+        .set(new_widget!(), ui)
       {
         $value = event;
         needs_update = true;
@@ -131,21 +138,19 @@ pub fn draw_gui(ids: &mut ChimperIds, ui: &mut UiCell, ops: &mut PipelineOps, id
   }
 
   voffset += 36.0 * 1.5;
-  divider_label!(ids.op_rawinput[11], "Levels");
+  divider_label!("Levels");
   voffset += 36.0 * 1.5;
-  label!(80.0, 150.0, ids.op_rawinput[12], "Min", Justify::Center);
-  label!(20.0, 250.0, ids.op_rawinput[13], "0", Justify::Left);
-  label!(170.0, 250.0, ids.op_rawinput[14], "Multiplier", Justify::Center);
-  label!(20.0, 400.0, ids.op_rawinput[15], "4", Justify::Right);
-  label!(80.0, 440.0, ids.op_rawinput[16], "Max", Justify::Center);
-  let mut idnum = 17;
+  label!(80.0,  150.0, "Min", Justify::Center);
+  label!(20.0,  250.0, "0", Justify::Left);
+  label!(170.0, 250.0, "Multiplier", Justify::Center);
+  label!(20.0,  400.0, "4", Justify::Right);
+  label!(80.0,  440.0, "Max", Justify::Center);
   macro_rules! range_widget {
     ($name:expr, $idx:expr) => {
-      idnum += 4;
-      left_label!(ids.op_rawinput[idnum], $name);
-      textbox_num_input!(150.0, voffset, ids.op_rawinput[idnum+1], ops.level.blacklevels[$idx], f32);
-      slider_input!(250.0, voffset, ids.op_rawinput[idnum+2], ops.level.wb_coeffs[$idx], 0.0, 4.0, f32);
-      textbox_num_input!(440.0, voffset, ids.op_rawinput[idnum+3], ops.level.whitelevels[$idx], f32);
+      left_label!($name);
+      textbox_num_input!(150.0, ops.level.blacklevels[$idx], f32);
+      slider_input!(250.0, ops.level.wb_coeffs[$idx], 0.0, 4.0, f32);
+      textbox_num_input!(440.0, ops.level.whitelevels[$idx], f32);
 
       voffset += 36.0;
     };

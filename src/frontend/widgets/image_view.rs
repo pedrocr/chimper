7bfffda6ada094afb0324fda885786cedb42d500
Pedro Côrte-Real <pedro@pedrocr.net>
2021-06-20 18:43:10 +0100

    Only show crop interface when initialized for it

--- src/frontend/widgets/image_view.rs
+++ src/frontend/widgets/image_view.rs
@@ -121,122 +121,125 @@ impl Widget for ImageView {
       ..
     } = self;
 
-    let mut crop_top = 0.0;
-    let mut crop_right = 0.0;
-    let mut crop_bottom = 0.0;
-    let mut crop_left = 0.0;
-    if let Some(crops) = crops {
-      crop_top = crops.0;
-      crop_right = crops.1;
-      crop_bottom = crops.2;
-      crop_left = crops.3;
-    }
-
-    let [width, height] = rect.dim();
-    let handle_size = if width > height { width } else { height } * 0.1;
-    let iwidth = width - handle_size * 2.0;
-    let iheight = height - handle_size * 2.0;
-    let range_x = (0.0 - (handle_size / iwidth), 1.0 + (handle_size / iwidth));
-    let range_y = (0.0 - (handle_size / iheight), 1.0 + (handle_size / iheight));
-
-    let mut highlight = ResizeMode::None;
     let mut event = None;
-    if let Some(mouse) = ui.widget_input(id).mouse() {
-      let mouse_abs_xy = mouse.abs_xy();
-      let clamped_x = rect.x.clamp_value(mouse_abs_xy[0]);
-      let clamped_y = rect.y.clamp_value(mouse_abs_xy[1]);
-      let (l, r, b, t) = rect.l_r_b_t();
-      let new_x = map_range(clamped_x, l, r, range_x.0, range_x.1);
-      let new_y = map_range(clamped_y, t, b, range_y.0, range_y.1);
-      if let Some(drag) = state.drag {
-        highlight = drag.mode;
-        let delta_x = new_x - drag.x;
-        let delta_y = new_y - drag.y;
-        macro_rules! drag_calc {
-          ($mode:ident, $crop:ident, $crop_opposite:ident, $drag:expr) => {{
-            if drag.mode.$mode() {
-              $crop += $drag;
-              if $crop < 0.0 {$crop = 0.0};
-              let steal = -(1.0 - $crop - $crop_opposite - MIN_SIZE);
-              if steal > 0.0 {
-                $crop_opposite -= steal;
-              }
-              if $crop_opposite < 0.0 {
-                $crop += $crop_opposite;
-                $crop_opposite = 0.0;
-              }
-              if $crop < 0.0 {$crop = 0.0};
-            }
-          }}
-        }
-        drag_calc!(left, crop_left, crop_right, delta_x);
-        drag_calc!(right, crop_right, crop_left, -delta_x);
-        drag_calc!(top, crop_top, crop_bottom, delta_y);
-        drag_calc!(bottom, crop_bottom, crop_top, -delta_y);
-        if !mouse.buttons.left().is_down() {
-          // We're no longer clicking so reset the state
-          state.update(|state| state.drag = None);
-          event = Some((crop_top, crop_right, crop_bottom, crop_left));
-        }
-      } else {
-        if new_x < 0.0+crop_left && new_y < 0.0+crop_top {
-          highlight = ResizeMode::TopLeft;
-        } else if new_x < 0.0+crop_left && new_y > 1.0-crop_bottom {
-          highlight = ResizeMode::BottomLeft;
-        } else if new_x > 1.0-crop_right && new_y < 0.0+crop_top {
-          highlight = ResizeMode::TopRight;
-        } else if new_x > 1.0-crop_right && new_y > 1.0-crop_bottom {
-          highlight = ResizeMode::BottomRight;
-        } else if new_x < 0.0+crop_left {
-          highlight = ResizeMode::Left;
-        } else if new_y < 0.0+crop_top {
-          highlight = ResizeMode::Top;
-        } else if new_x > 1.0-crop_right {
-          highlight = ResizeMode::Right;
-        } else if new_y > 1.0-crop_bottom {
-          highlight = ResizeMode::Bottom;
-        }
+    if let Some(crops) = crops {
+      let mut crop_top = crops.0;
+      let mut crop_right = crops.1;
+      let mut crop_bottom = crops.2;
+      let mut crop_left = crops.3;
 
-        if mouse.buttons.left().is_down() && highlight != ResizeMode::None {
-          // We're in the first click so initiate a drag
-          state.update(|state| state.drag = Some(Drag {
-            x: new_x,
-            y: new_y,
-            mode: highlight,
-          }));
+      let [width, height] = rect.dim();
+      let handle_size = if width > height { width } else { height } * 0.1;
+      let iwidth = width - handle_size * 2.0;
+      let iheight = height - handle_size * 2.0;
+      let range_x = (0.0 - (handle_size / iwidth), 1.0 + (handle_size / iwidth));
+      let range_y = (0.0 - (handle_size / iheight), 1.0 + (handle_size / iheight));
+
+      let mut highlight = ResizeMode::None;
+      if let Some(mouse) = ui.widget_input(id).mouse() {
+        let mouse_abs_xy = mouse.abs_xy();
+        let clamped_x = rect.x.clamp_value(mouse_abs_xy[0]);
+        let clamped_y = rect.y.clamp_value(mouse_abs_xy[1]);
+        let (l, r, b, t) = rect.l_r_b_t();
+        let new_x = map_range(clamped_x, l, r, range_x.0, range_x.1);
+        let new_y = map_range(clamped_y, t, b, range_y.0, range_y.1);
+        if let Some(drag) = state.drag {
+          highlight = drag.mode;
+          let delta_x = new_x - drag.x;
+          let delta_y = new_y - drag.y;
+          macro_rules! drag_calc {
+            ($mode:ident, $crop:ident, $crop_opposite:ident, $drag:expr) => {{
+              if drag.mode.$mode() {
+                $crop += $drag;
+                if $crop < 0.0 {$crop = 0.0};
+                let steal = -(1.0 - $crop - $crop_opposite - MIN_SIZE);
+                if steal > 0.0 {
+                  $crop_opposite -= steal;
+                }
+                if $crop_opposite < 0.0 {
+                  $crop += $crop_opposite;
+                  $crop_opposite = 0.0;
+                }
+                if $crop < 0.0 {$crop = 0.0};
+              }
+            }}
+          }
+          drag_calc!(left, crop_left, crop_right, delta_x);
+          drag_calc!(right, crop_right, crop_left, -delta_x);
+          drag_calc!(top, crop_top, crop_bottom, delta_y);
+          drag_calc!(bottom, crop_bottom, crop_top, -delta_y);
+          if !mouse.buttons.left().is_down() {
+            // We're no longer clicking so reset the state
+            state.update(|state| state.drag = None);
+            event = Some((crop_top, crop_right, crop_bottom, crop_left));
+          }
+        } else {
+          if new_x < 0.0+crop_left && new_y < 0.0+crop_top {
+            highlight = ResizeMode::TopLeft;
+          } else if new_x < 0.0+crop_left && new_y > 1.0-crop_bottom {
+            highlight = ResizeMode::BottomLeft;
+          } else if new_x > 1.0-crop_right && new_y < 0.0+crop_top {
+            highlight = ResizeMode::TopRight;
+          } else if new_x > 1.0-crop_right && new_y > 1.0-crop_bottom {
+            highlight = ResizeMode::BottomRight;
+          } else if new_x < 0.0+crop_left {
+            highlight = ResizeMode::Left;
+          } else if new_y < 0.0+crop_top {
+            highlight = ResizeMode::Top;
+          } else if new_x > 1.0-crop_right {
+            highlight = ResizeMode::Right;
+          } else if new_y > 1.0-crop_bottom {
+            highlight = ResizeMode::Bottom;
+          }
+
+          if mouse.buttons.left().is_down() && highlight != ResizeMode::None {
+            // We're in the first click so initiate a drag
+            state.update(|state| state.drag = Some(Drag {
+              x: new_x,
+              y: new_y,
+              mode: highlight,
+            }));
+          }
         }
       }
+
+      let color = RED.with_alpha(0.1);
+      let color_highlight = RED.with_alpha(0.5);
+
+      widget::Image::new(image_id)
+        .middle_of(id)
+        .w_h(iwidth, iheight)
+        .graphics_for(id)
+        .set(state.ids.image, ui);
+      widget::primitive::shape::rectangle::Rectangle::fill([handle_size, height])
+        .top_left_with_margins_on(id, 0.0, iwidth * crop_left)
+        .graphics_for(id)
+        .color(if highlight.left() {color_highlight} else {color})
+        .set(state.ids.handles[0], ui);
+      widget::primitive::shape::rectangle::Rectangle::fill([handle_size, height])
+        .top_right_with_margins_on(id, 0.0, iwidth * crop_right)
+        .graphics_for(id)
+        .color(if highlight.right() {color_highlight} else {color})
+        .set(state.ids.handles[1], ui);
+      widget::primitive::shape::rectangle::Rectangle::fill([width, handle_size])
+        .top_left_with_margins_on(id, iheight * crop_top, 0.0)
+        .graphics_for(id)
+        .color(if highlight.top() {color_highlight} else {color})
+        .set(state.ids.handles[2], ui);
+      widget::primitive::shape::rectangle::Rectangle::fill([width, handle_size])
+        .bottom_left_with_margins_on(id, iheight * crop_bottom, 0.0)
+        .graphics_for(id)
+        .color(if highlight.bottom() {color_highlight} else {color})
+        .set(state.ids.handles[3], ui);
+    } else {
+      // We weren't initialized in crop mode so just show the image
+      let [width, height] = rect.dim();
+      widget::Image::new(image_id)
+        .middle_of(id)
+        .w_h(width, height)
+        .graphics_for(id)
+        .set(state.ids.image, ui);
     }
-
-    let color = RED.with_alpha(0.1);
-    let color_highlight = RED.with_alpha(0.5);
-
-    widget::Image::new(image_id)
-      .middle_of(id)
-      .w_h(iwidth, iheight)
-      .graphics_for(id)
-      .set(state.ids.image, ui);
-    widget::primitive::shape::rectangle::Rectangle::fill([handle_size, height])
-      .top_left_with_margins_on(id, 0.0, iwidth * crop_left)
-      .graphics_for(id)
-      .color(if highlight.left() {color_highlight} else {color})
-      .set(state.ids.handles[0], ui);
-    widget::primitive::shape::rectangle::Rectangle::fill([handle_size, height])
-      .top_right_with_margins_on(id, 0.0, iwidth * crop_right)
-      .graphics_for(id)
-      .color(if highlight.right() {color_highlight} else {color})
-      .set(state.ids.handles[1], ui);
-    widget::primitive::shape::rectangle::Rectangle::fill([width, handle_size])
-      .top_left_with_margins_on(id, iheight * crop_top, 0.0)
-      .graphics_for(id)
-      .color(if highlight.top() {color_highlight} else {color})
-      .set(state.ids.handles[2], ui);
-    widget::primitive::shape::rectangle::Rectangle::fill([width, handle_size])
-      .bottom_left_with_margins_on(id, iheight * crop_bottom, 0.0)
-      .graphics_for(id)
-      .color(if highlight.bottom() {color_highlight} else {color})
-      .set(state.ids.handles[3], ui);
-
     event
   }
 }
